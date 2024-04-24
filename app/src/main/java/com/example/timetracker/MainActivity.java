864615c27d0b7f71f8c34bc09bda5e1ee6e25777
Sancheez10 <danisecundaria99@gmail.com>
2024-04-24 20:35:15 +0200

    pa dani

--- app/src/main/java/com/example/timetracker/MainActivity.java
+++ app/src/main/java/com/example/timetracker/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.timetracker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.Manifest;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private Button getLocationButton;
+    private LocationManager locationManager;
     private Toolbar toolbar_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +34,83 @@ public class MainActivity extends AppCompatActivity {
         toolbar_main = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar_main);
 
-    }
+        getLocationButton = findViewById(R.id.getLocationButton);
+        getLocationButton.setOnClickListener(this);
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        checkLocationSettings();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar, menu);
         return true;
     }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == getLocationButton) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                checkLocationSettingsAndShowMessage();
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new MyLocationListener());
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
+
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            TextView tvCoordinates = findViewById(R.id.getLocationButton);
+            tvCoordinates.setText("Ubicación: " + latitude + ", " + longitude);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+    }
+
+    private void checkLocationSettings() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!isGpsEnabled) {
+            getLocationButton.setText("Sin ubicación");
+        } else {
+            getLocationButton.setText("Obtener ubicación");
+        }
+    }
+
+    private void checkLocationSettingsAndShowMessage() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!isGpsEnabled) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Ubicación desactivada")
+                    .setMessage("Por favor, active la ubicación para obtener su ubicación.")
+                    .setPositiveButton("Activar ubicación", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton("Cancelar", null)
+                    .create()
+                    .show();
+        }
+    }
+
+
+
 }
