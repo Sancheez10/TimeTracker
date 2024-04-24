1c9ad6f560c975040d7e570d9f8866a1219ea926
Sancheez10 <danisecundaria99@gmail.com>
2024-04-24 19:33:28 +0200

    TOOLBAR

--- app/src/main/java/com/example/timetracker/MainActivity.java
+++ app/src/main/java/com/example/timetracker/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.timetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar_main = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar_main);
 
     }
 }
