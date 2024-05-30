f5ce4c88de5972595ddf6e8ac1e8cc6fb368e925
Sancheez10 <danisecundaria99@gmail.com>
2024-05-30 20:50:45 +0200

    pa dani

--- app/src/main/java/com/example/timetracker/AuthActivity.java
+++ app/src/main/java/com/example/timetracker/AuthActivity.java
@@ -1,8 +1,9 @@
-
 package com.example.timetracker;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -19,13 +20,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-
 public class AuthActivity extends AppCompatActivity {
 
     private EditText emailEditText, passwordEditText;
     private Button loginButton, signUpButton;
     private FirebaseAuth firebaseAuth;
     private DatabaseReference databaseRef;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +39,13 @@ public class AuthActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.loginButton);
         signUpButton = findViewById(R.id.signUpButton);
 
-        // Inicializar Firebase Authentication
+        // Inicializar Firebase Authentication y la referencia a la base de datos
         firebaseAuth = FirebaseAuth.getInstance();
-
         databaseRef = FirebaseDatabase.getInstance().getReference("Users");
+
+        // Inicializar SharedPreferences
+        sharedPreferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
+
         // Configurar los listeners de los botones
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,7 +57,7 @@ public class AuthActivity extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Aquí puedes agregar la lógica para redirigir al usuario a la pantalla de registro
+                signUpUser();
             }
         });
     }
@@ -79,11 +83,43 @@ public class AuthActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Inicio de sesión exitoso
                             String userId = firebaseAuth.getCurrentUser().getUid();
-                            saveUserData(userId, email, password);
-                            Toast.makeText(AuthActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                            saveUserDataInPreferences(userId, email);
+                            redirectToMainActivity();
                         } else {
                             // Fallo en el inicio de sesión, muestra un mensaje de error
                             Toast.makeText(AuthActivity.this, "Fallo en el inicio de sesión: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signUpUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            emailEditText.setError("Ingresa un correo electrónico");
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            passwordEditText.setError("Ingresa una contraseña");
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Registro exitoso
+                            String userId = firebaseAuth.getCurrentUser().getUid();
+                            saveUserData(userId, email, password);
+                            saveUserDataInPreferences(userId, email);
+                            redirectToMainActivity();
+                        } else {
+                            // Fallo en el registro, muestra un mensaje de error
+                            Toast.makeText(AuthActivity.this, "Fallo en el registro: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
@@ -94,7 +130,7 @@ public class AuthActivity extends AppCompatActivity {
         Person user = new Person(email, password);
 
         // Guardar los datos del usuario en la base de datos
-        databaseRef.child("users").child(userId).setValue(user)
+        databaseRef.child(userId).setValue(user)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
@@ -109,8 +145,16 @@ public class AuthActivity extends AppCompatActivity {
                 });
     }
 
+    private void saveUserDataInPreferences(String userId, String email) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("userId", userId);
+        editor.putString("email", email);
+        editor.apply();
+    }
 
-
-
+    private void redirectToMainActivity() {
+        Intent intent = new Intent(AuthActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
-
