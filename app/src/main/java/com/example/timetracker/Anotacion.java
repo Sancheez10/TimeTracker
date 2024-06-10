fd0ab39afcc9d62687b45b0bd6c42ae92ead4552
Usuario-PC\Usuario <d.diaz@nextt.es>
2024-06-10 19:30:57 +0200

    anotaciones

--- app/src/main/java/com/example/timetracker/Anotacion.java
+++ app/src/main/java/com/example/timetracker/Anotacion.java
@@ -3,21 +3,23 @@ package com.example.timetracker;
 import java.util.Date;
 
 public class Anotacion {
-    private String id;
-    private String texto;
-    private Date fechaHora;
-    private String fileUrl; // URL del fichero adjunto
-    private String createdBy; // Trabajador que la creó
+    private String id; // ID del documento en Firestore
+    private String texto; // Texto de la anotación
+    private Date fechaHora; // Fecha y hora de la anotación
+    private String fileUrl; // URL del archivo adjunto, si lo hay
+    private String createdBy; // Usuario que creó la anotación
+    private boolean isViewed; // Indicador de si la anotación ha sido vista
 
     // Constructor vacío necesario para Firebase
     public Anotacion() {}
 
-    public Anotacion(String id, String texto, Date fechaHora, String fileUrl, String createdBy) {
+    public Anotacion(String id, String texto, Date fechaHora, String fileUrl, String createdBy, boolean isViewed) {
         this.id = id;
         this.texto = texto;
         this.fechaHora = fechaHora;
         this.fileUrl = fileUrl;
         this.createdBy = createdBy;
+        this.isViewed = isViewed;
     }
 
     // Getters y setters
@@ -60,5 +62,13 @@ public class Anotacion {
 
     public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
+    }
+
+    public boolean isViewed() {
+        return isViewed;
+    }
+
+    public void setViewed(boolean viewed) {
+        isViewed = viewed;
     }
 }
